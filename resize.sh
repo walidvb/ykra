2c1dcecd6c4dd98b082a2820d70896e7cdeacbb1
Walid <walidvb@gmail.com>
2014-08-05 20:25:53 +0200

    adds resize script

--- resize.sh
+++ resize.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-cd ~/Sites/ykra/hiRes
-FILES=`ls`
-echo $FILES
-
+cd "$1"
+FILES=``
 if ! [ -a lowRes ] 
 	then
 		echo "Folder lowRes not found, creating it"
 		mkdir lowRes
 fi
+find "$1" -type f | while read f ; do
+	if [ -f "$f" ] 
+		then 
+			FILENAME=`basename "$f"`
+			echo "Processing "$FILENAME" to "lowRes/$FILENAME""
 
-for f in $FILES 
-	do
-		if [ -f $f ] 
-			then 
-				echo "Processing $f"
-				sips -Z 1280 $f --out lowRes/$f
-		fi
-	done
+			sips -Z 1280 "$f" --out "lowRes/$FILENAME"
+	else
+		echo "Skipping $f"
+	fi
+done
 
-	echo "All images were resized to 1280"
+echo "All images were resized to 1280"
+exit
